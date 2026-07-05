21c32b8b2e6c16a7d2ad03ec74a4ba68001a7268
Olvin_Martinez <martinezfunezolvinjavier@gmail.com>
2026-07-04 20:40:52 -0600

    Agregar portafolio completo

---
+++ src/data/pages/index_page.js
@@ -0,0 +1,36 @@
+export default {
+    planes: [
+        {
+            card_title: "La Casa del PVC",
+            card_description: "Sistema académico orientado al análisis de módulos, procesos y estructura de un negocio.",
+            card_img: "https://picsum.photos/id/180/300/200",
+            card_label: "Análisis y diseño",
+            card_price: "HTML / Diagramas",
+            card_cta: "Ver proyecto",
+        },
+        {
+            card_title: "Ramona Tejeda",
+            card_description: "Base de datos escolar en MongoDB con niveles, grados, estudiantes, docentes, pagos y matrículas.",
+            card_img: "https://picsum.photos/id/20/300/200",
+            card_label: "Base de datos",
+            card_price: "MongoDB",
+            card_cta: "Ver proyecto",
+        },
+        {
+            card_title: "Mapa de Juticalpa",
+            card_description: "Página interactiva sobre lugares, barrios, restaurantes y puntos importantes de Juticalpa.",
+            card_img: "https://picsum.photos/id/1015/300/200",
+            card_label: "Proyecto web",
+            card_price: "HTML / CSS / JS",
+            card_cta: "Ver proyecto",
+        },
+        {
+            card_title: "Portafolio Web",
+            card_description: "Portafolio desarrollado con Vite, LESS, Handlebars, CSS purgado y archivos minificados.",
+            card_img: "https://picsum.photos/id/0/300/200",
+            card_label: "Frontend",
+            card_price: "Vite / LESS",
+            card_cta: "Ver proyecto",
+        }
+    ]
+};
