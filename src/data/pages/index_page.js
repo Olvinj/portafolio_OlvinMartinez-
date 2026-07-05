075191ef50618724856ddc11c23e9e3750086025
Olvin_Martinez <martinezfunezolvinjavier@gmail.com>
2026-07-04 22:22:27 -0600

    Primer commit

--- src/data/pages/index_page.js
+++ src/data/pages/index_page.js
@@ -1,36 +1,28 @@
 export default {
     planes: [
         {
-            card_title: "La Casa del PVC",
-            card_description: "Sistema académico orientado al análisis de módulos, procesos y estructura de un negocio.",
-            card_img: "https://picsum.photos/id/180/300/200",
-            card_label: "Análisis y diseño",
-            card_price: "HTML / Diagramas",
-            card_cta: "Ver proyecto",
-        },
-        {
-            card_title: "Ramona Tejeda",
-            card_description: "Base de datos escolar en MongoDB con niveles, grados, estudiantes, docentes, pagos y matrículas.",
-            card_img: "https://picsum.photos/id/20/300/200",
-            card_label: "Base de datos",
-            card_price: "MongoDB",
-            card_cta: "Ver proyecto",
-        },
-        {
-            card_title: "Mapa de Juticalpa",
-            card_description: "Página interactiva sobre lugares, barrios, restaurantes y puntos importantes de Juticalpa.",
-            card_img: "https://picsum.photos/id/1015/300/200",
-            card_label: "Proyecto web",
-            card_price: "HTML / CSS / JS",
-            card_cta: "Ver proyecto",
-        },
-        {
-            card_title: "Portafolio Web",
-            card_description: "Portafolio desarrollado con Vite, LESS, Handlebars, CSS purgado y archivos minificados.",
-            card_img: "https://picsum.photos/id/0/300/200",
+            card_title: "Diseño Web",
+            card_description: "Creación de páginas modernas, responsivas y optimizadas para presentar negocios, marcas o proyectos.",
+            card_img: "https://picsum.photos/id/26/400/260",
             card_label: "Frontend",
-            card_price: "Vite / LESS",
-            card_cta: "Ver proyecto",
+            card_price: "HTML / LESS / JS",
+            card_cta: "Más información",
+        },
+        {
+            card_title: "Sistemas Empresariales",
+            card_description: "Desarrollo de sistemas para controlar ventas, clientes, productos, inventarios y procesos internos.",
+            card_img: "https://picsum.photos/id/60/400/260",
+            card_label: "Software",
+            card_price: "JavaScript / Java",
+            card_cta: "Más información",
+        },
+        {
+            card_title: "Bases de Datos",
+            card_description: "Diseño y organización de bases de datos relacionales y no relacionales para proyectos académicos o reales.",
+            card_img: "https://picsum.photos/id/48/400/260",
+            card_label: "Data",
+            card_price: "MySQL / MongoDB",
+            card_cta: "Más información",
         }
     ]
 };
